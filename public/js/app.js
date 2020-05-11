07f6516fcbfd65f9df2ce79523d1b7ff88d978bd
Vinayak Kalunge <vinayak.kalunge@quest-global.com>
2020-05-11 08:44:55 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,11 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')      //document.querySelector({element name}) get the first html element that it founds
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')  //Note: for accessing html element by id - use #{id value}  // for accessing html element by class - use .{classname}
+const messageOne = document.querySelector('#message-1')  //Note: for accessing html element: by id - use #{id value}, by class - use .{classname}
 const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (event) => {
                                             //Note: Default behavior of form is to completely reload the page  
-    event.preventDefault()                  //by setting this we've told browser that we're gonna handle everything, it doesn't need to do what it usually does.
+    event.preventDefault()                  //by setting this we've A   `Q1told browser that we're gonna handle everything, it doesn't need to do what it usually does.
     
     const location = search.value           //get value from the html element
 
@@ -23,8 +21,7 @@ weatherForm.addEventListener('submit', (event) => {
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forcast
-            }
-            
+            }  
         })
     })
 })
